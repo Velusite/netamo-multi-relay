16e1e8d350727ccb95cc412515ac97b758eef461
Velusite <115937996+Velusite@users.noreply.github.com>
2022-11-22 22:25:24 +0100

    hop

--- NetAtmo-MultiRelay.cs
+++ NetAtmo-MultiRelay.cs
@@ -14,26 +14,29 @@ namespace Gentilhommiere.NetAtmoMultiRelay
         }
 
         [Function("NetAtmo_MultiRelay")]
-        public void Run([TimerTrigger("0 */6 * * * *")] MyInfo myTimer)
+        public void Run([TimerTrigger("0 */6 * * * *")] MyTimer myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            _logger.LogInformation($"Next timer schedule at: {myTimer}");
         }
     }
 
-    public class MyInfo
+    public class MyTimer
     {
-        public MyScheduleStatus ScheduleStatus { get; set; }
-
+        public MyScheduleStatus? ScheduleStatus { get; set; }
+        public MySchedule? Schedule { get; set; }
         public bool IsPastDue { get; set; }
+    }
+
+    public class MySchedule
+    {
+        public bool AdjustForDST { get; set; }
     }
 
     public class MyScheduleStatus
     {
         public DateTime Last { get; set; }
-
         public DateTime Next { get; set; }
-
         public DateTime LastUpdated { get; set; }
     }
 }
